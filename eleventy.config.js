85abfd13e614258959db77a4be0db0228624064d
Takayuki Kumagai <xtakayukikumagai@gmail.com>
2025-12-14 18:54:41 +0900

    second commit

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,10 +2,15 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = function (eleventyConfig) {
   // SCSSコンパイル後のCSSファイルをコピー
-  eleventyConfig.addPassthroughCopy("dist/css");
+  eleventyConfig.addPassthroughCopy({ "dist/css": "css" });
   // Pugコンパイル後のHTMLファイルをコピー（必要な場合）
-  eleventyConfig.addPassthroughCopy("dist/html");
+  eleventyConfig.addPassthroughCopy({ "dist/html": "html" });
 
   // その他の設定
-  // ...
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+    },
+  };
 };
