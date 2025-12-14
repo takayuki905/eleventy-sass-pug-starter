82e78b4e43310a8140b8ab85b1a7583742a86b91
Takayuki Kumagai <xtakayukikumagai@gmail.com>
2025-12-14 21:59:51 +0900

    copy static files

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,7 @@ module.exports = function (eleventyConfig) {
   // SCSSコンパイル後のCSSファイルをコピー
   eleventyConfig.addPassthroughCopy({ "dist/css": "css" });
   // Pugコンパイル後のHTMLファイルをコピー（必要な場合）
-  eleventyConfig.addPassthroughCopy({ "dist/html": "html" });
+  eleventyConfig.addPassthroughCopy({ dist: "." });
 
   // その他の設定
   return {
